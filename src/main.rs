ca528cdf9685472d8bd3ad3ea7a5de91ffc37273
Tyler Kordziel <tskordziel@gmail.com>
2025-11-22 15:34:28 -0700

    add SudokuBoard struct

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,6 @@ struct Cell {
     value: Option<Color>,
     possible_values: HashSet<Color>
 }
-
 impl Cell {
     fn new() -> Cell{
         let mut  colors_set: HashSet<Color> = HashSet::new();
@@ -30,6 +29,28 @@ impl Cell {
         Cell { value: None, possible_values: colors_set }
     }
 }
+struct SudokuBoard {
+    grid: [[Cell; 9]; 9],
+    constraints: UnGraph<(usize, usize), ()>
+}
+
+impl SudokuBoard {
+    fn new() -> SudokuBoard{
+        let mut board: SudokuBoard = SudokuBoard { grid: [[Cell::new(); 9]; 9], constraints: UnGraph::new_undirected() };
+
+        for (i,row) in board.grid.iter().enumerate(){
+            for (j, colum) in row.iter().enumerate(){
+                board.constraints.add_node((i,j));
+            }
+        }
+        
+    }
+    
+}
+
+
+
+
 
 fn main() {
     println!("Hello, world!");
